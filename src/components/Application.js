b1033dd84c1c581a54b991745deb28d944299977
Alvin Lin <alvinch.lin@gmail.com>
2021-05-13 23:09:43 -0700

    Implement getInterview selector fucntion. Test the getinterview selector by unit testing. App is now able to fetch interviewers data.

--- src/components/Application.js
+++ src/components/Application.js
@@ -4,26 +4,27 @@ import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "./Appointment";
 import axios from 'axios';
-import getAppointmentsForDay from '../helpers/selectors';
+import {getAppointmentsForDay, getInterview} from '../helpers/selectors';
 
 export default function Application(props) {
   const [state, setState] = useState({
     daySelected: "Monday",
     days: [],
-    appointments:{}
+    appointments:{},
+    interviewers:{}
   })
+  console.log(state.interviewers);
   const setSelectedDay = daySelected => setState({...state, daySelected });
-  // const setDays = days => setState(prev => ({...prev, days}))
   const dailyAppointments = getAppointmentsForDay(state, state.daySelected);
 
   useEffect(()=> {
     Promise.all([
       axios.get("/api/days"),
-      axios.get("/api/appointments")
+      axios.get("/api/appointments"),
+      axios.get("/api/interviewers")
     ])
     .then(all => {
-      console.log(all[1].data);
-      setState(prev => ({...prev, days:all[0].data, appointments:all[1].data}))
+      setState(prev => ({...prev, days:all[0].data, appointments:all[1].data, interviewers:all[2].data}))
     })
 
   },[])
@@ -52,12 +53,15 @@ export default function Application(props) {
       </section>
       <section className="schedule">
         {dailyAppointments.map(appointment => {
+          const interview = getInterview(state, appointment.interview);
+          console.log(appointment.interview);
           return (
           <Appointment
             key={appointment.id}
-            {...appointment}
-            // time={appointment.time}
-            // interview={appointment.interview}
+            // {...appointment}
+            id={appointment.id}
+            time={appointment.time}
+            interview={interview}
           />
           )
         })}
--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,14 +1,27 @@
-export default function getAppointmentsForDay (state, day) {
-  const result = []
-  const filterByDay = state.days.filter(dayObj => {
-    return dayObj.name === day
-  })
-  
+const getAppointmentsForDay = (state, day) => {
+  const result = [];
+  const filterByDay = state.days.filter((dayObj) => {
+    return dayObj.name === day;
+  });
+
   if (filterByDay.length !== 0) {
     const appointmentArr = filterByDay[0].appointments;
-    for (const apptID of appointmentArr ) {
-       result.push(state.appointments[apptID])
+    for (const apptID of appointmentArr) {
+      result.push(state.appointments[apptID]);
     }
   }
   return result;
-}
+};
+
+const getInterview = function (state, interviewObj) {
+  let result = null;
+  if (interviewObj) {
+    const interviewerID = interviewObj.interviewer;
+    const interviewerInfo = state.interviewers[interviewerID];
+    result = { ...interviewObj, interviewer: interviewerInfo };
+  }
+
+  return result;
+};
+
+export { getAppointmentsForDay, getInterview };

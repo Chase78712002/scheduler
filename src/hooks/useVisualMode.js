76b64eb8c78ae4e53405f0c9b68aea70b8e2f392
Alvin Lin <alvinch.lin@gmail.com>
2021-05-13 23:10:32 -0700

    Implement useVisualMode hook. Test useVisualMode hook. test all pass.

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,29 @@
+import { useState } from "react";
+
+const useVisualMode = (initial) => {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+
+  const transition = (newMode, replace = false) => {
+    if (!replace) {
+      setHistory((prev) => [...prev, newMode]);
+    }
+    setMode(newMode);
+  };
+  const back = () => {
+    if (history.length >  1) {
+      setHistory((prev) => {
+        const reversedHistory = [...prev].slice(0, -1);
+        const previousMode = reversedHistory.slice(-1)[0];
+        // console.log("reversedHistory: ", reversedHistory);
+        // console.log("previousMode: ", previousMode);
+        setMode(previousMode);
+        return reversedHistory;
+      });
+    }
+  };
+
+  return { mode, transition, back };
+};
+
+export default useVisualMode;

da5c5e2f1557108ce09ba4f08e10ca95b1e5b5af
Alvin Lin <alvinch.lin@gmail.com>
2021-05-13 23:11:36 -0700

    Working on implementing the useVisualMode hook into the Appointment component

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -5,10 +5,17 @@ import Show from "./Show";
 import Empty from "./Empty";
 import { action } from "@storybook/addon-actions/dist/preview";
 
+import useVisualMode from '../../hooks/useVisualMode';
+
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
 export default function Appointment(props) {
   const { time, interview } = props;
+  const {mode, transition, back} = useVisualMode(interview ? SHOW:EMPTY);
+
   return (
     <article className="appointment">
+      
       <Header time={time} />
       {interview ? (
         <Show 
